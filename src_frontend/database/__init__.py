13267a7c07eb5dcd9bc1c4964ea6c23efa096ed3
schleising <stephen@schleising.net>
2023-06-25 16:57:29 +0100

    Create indexes on the fields which are used to filter data

--- src_frontend/database/__init__.py
+++ src_frontend/database/__init__.py
@@ -27,3 +27,13 @@ _db: AIOMDB = _client["media"]
 
 # Get the media collection
 media_collection: AIOMC = _db.get_collection("media_collection", codec_options=CodecOptions(tz_aware=True))
+
+# Create indexes
+media_collection.create_index("conversion_required")
+media_collection.create_index("converting")
+media_collection.create_index("converted")
+media_collection.create_index("conversion_error")
+media_collection.create_index("start_conversion_time")
+media_collection.create_index("end_conversion_time")
+media_collection.create_index("pre_conversion_size")
+media_collection.create_index("current_size")
